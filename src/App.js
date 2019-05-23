6d67e6e7173e27e96eab5f29a54e3929b776abb9
Christopher Gonzalez <christopher_gonzalez@homedepot.com>
2019-05-23 15:44:15 -0500

    d

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => <StateOfMatter />;
+
+const MatterContext = React.createContext({});
+const MatterProvider = MatterContext.Provider;
+const MatterConsumer = MatterContext.Consumer;
+
+class StateOfMatter extends React.Component {
+  state = {
+    element: "Oxygen"
+  };
+
+  render() {
+    return (
+      <div className="stateOfMatter">
+        <MatterProvider value={this.state.element}>
+          <Solid />
+        </MatterProvider>
+      </div>
+    );
+  }
 }
+
+const Solid = () => {
+  return <Liquid />;
+};
+
+const Liquid = () => {
+  return <Gas />;
+};
+
+const Gas = () => {
+  return <Plasma />;
+};
+
+const Plasma = () => {
+  return <MatterConsumer>{context => <p>{context}</p>}</MatterConsumer>;
+};
 
 export default App;
